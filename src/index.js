bc72f24d83e4180a7a5eb445c04994a255df0569
Adrian Kahiga <kahigaadrian7@gmail.com>
2025-03-25 07:09:26 +0300

    added click event to display character details

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 // Your code here
 document.addEventListener("DOMContentLoaded", async () => {
-    const response = await fetch("db.json");
-    const data = await response.json();
-    const characterBar = document.getElementById("character-bar");
-
-    data.characters.forEach(character => {
+        const response = await fetch("db.json");
+        const data = await response.json();
+        const characterBar = document.getElementById("character-bar");
+        data.characters.forEach(character => {
         const span = document.createElement("span");
         span.textContent = character.name;
-        characterBar.appendChild(span);});});
+                
+span.addEventListener("click", () => {
+    displayCharacterDetails(character);});
+    characterBar.appendChild(span);});
+    function displayCharacterDetails(character) {
+    document.getElementById("name").textContent = character.name;
+    document.getElementById("image").src = character.image;
+    document.getElementById("image").alt = character.name;
+    document.getElementById("vote-count").textContent = character.votes;}});        
